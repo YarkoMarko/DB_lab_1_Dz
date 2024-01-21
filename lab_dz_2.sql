e97225764d15f60d3e83ae38818aab74241828f6
YarkoMarko <Yarema1310@gmail.com>
2024-01-21 21:47:22 +0200

    Task 5

--- lab_dz_2.sql
+++ lab_dz_2.sql
@@ -1,20 +1,9 @@
---CREATE DATABASE FrVeg;
--- CREATE TABLE Stuff(
--- 	element_id SERIAL PRIMARY KEY,
--- 	nameOF VARCHAR(50),
--- 	typeOF VARCHAR(50),
--- 	colour VARCHAR(50),
--- 	caloric_content INT,
--- 	description TEXT)
-
-INSERT INTO Stuff(
-	nameOF,
-	typeOF,
-	colour,
-	caloric_content,
-	description)
-	
-VALUES
-  ('Apple', 'Fruit', 'Red', 100, 'Description for Apple, Fruit, Red'),
-  ('Grapes', 'Fruit', 'Green', 150, 'Description for Grapes, Fruit, Green'),
-  ('Potato', 'Vegetable', 'Brown', 120, 'Description for Potato, Vegetable, Brown');
+--SELECT * FROM Stuff
+-- SELECT nameOF FROM STUFF
+-- WHERE typeOF = 'Vegetable'
+-- SELECT nameOF FROM STUFF
+-- WHERE typeOF = 'Fruit'
+-- SELECT nameOF FROM Stuff
+-- SELECT DISTINCT colour FROM Stuff
+-- SELECT nameOF FROM Stuff 
+-- WHERE typeOF = 'Fruit' AND colour = 'Red'
