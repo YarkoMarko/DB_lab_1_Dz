eef5009e15809083c812490911acdad143302feb
YarkoMarko <Yarema1310@gmail.com>
2024-01-21 21:34:34 +0200

    Task 4 rework

--- lab_dz_2.sql
+++ lab_dz_2.sql
@@ -1,9 +1,16 @@
 --CREATE DATABASE FrVeg;
 CREATE TABLE Stuff(
 	element_id SERIAL PRIMARY KEY,
+	nameOF VARCHAR(50),
 	typeOF VARCHAR(50),
 	colour VARCHAR(50),
 	caloric_content INT,
-	description TEXT
+	description TEXT)
+
+INSERT INTO Stuff(
+	nameOF,
+	typeOF,
+	colour,
+	caloric_content,
+	description)
 	
-)
