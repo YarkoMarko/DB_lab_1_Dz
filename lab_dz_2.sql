5d86f18374407ea4823c18cce765ca231bb83cdc
YarkoMarko <Yarema1310@gmail.com>
2024-01-21 21:37:48 +0200

    Task 4

--- lab_dz_2.sql
+++ lab_dz_2.sql
@@ -1,11 +1,11 @@
 --CREATE DATABASE FrVeg;
-CREATE TABLE Stuff(
-	element_id SERIAL PRIMARY KEY,
-	nameOF VARCHAR(50),
-	typeOF VARCHAR(50),
-	colour VARCHAR(50),
-	caloric_content INT,
-	description TEXT)
+-- CREATE TABLE Stuff(
+-- 	element_id SERIAL PRIMARY KEY,
+-- 	nameOF VARCHAR(50),
+-- 	typeOF VARCHAR(50),
+-- 	colour VARCHAR(50),
+-- 	caloric_content INT,
+-- 	description TEXT)
 
 INSERT INTO Stuff(
 	nameOF,
@@ -14,3 +14,7 @@ INSERT INTO Stuff(
 	caloric_content,
 	description)
 	
+VALUES
+  ('Apple', 'Fruit', 'Red', 100, 'Description for Apple, Fruit, Red'),
+  ('Grapes', 'Fruit', 'Green', 150, 'Description for Grapes, Fruit, Green'),
+  ('Potato', 'Vegetable', 'Brown', 120, 'Description for Potato, Vegetable, Brown');
